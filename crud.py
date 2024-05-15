7820171766cc94b701ce741be0144feb4f01e9e0
João Ramos <joao.ramos@chat2desk.com.br>
2024-05-15 13:28:45 -0300

    Crud Python Postgress

---
+++ crud.py
@@ -0,0 +1,120 @@
+import db_function
+from tabulate import tabulate
+import os
+
+# LIMPAR CONSOLE
+
+def limpar_console():
+    os.system('cls')
+
+
+# VOCÊ PODE ADICIONAR QUANTAS COLUNAS VOCÊ ACHAR NECESSARIO. BASTA ANTES DE RODAR O CODIGO ADICIONAR AS COLUNAS. A QUANTIDADE DE COLUNAS É A QUANTIDADE DE INPUTS QUE A APLICAÇÃO IRÁ PEDIR, ESTÁ TUDO DINAMICO.
+
+columnsTable = ["nome","salario","idade"] # COLUNAS DO BANCO DE DADOS
+nameTable = "crud_empregado" # NOME DA TABELA PARA CRIAR E INSERIR OS DADOS
+
+# CRIAR TABELA DE ACORDO COM O NOME DA TABELA E AS COLUNAS:
+
+db_function.dbCreateTable(nameTable,columnsTable)
+
+
+# CREATE
+def dbCriarPost(dadosCriarPost,dbColumns):
+
+    conn, cursor = db_function.dbConection()
+
+    dadosCriarPostComAspas = ["'{}'".format(item) for item in dadosCriarPost]
+    cursor.execute("""INSERT INTO public.{} ({}) VALUES ({});""".format(nameTable,", ".join(dbColumns),", ".join(dadosCriarPostComAspas)))
+     
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+# READ
+def dbLerPosts(verificarId = None):
+    conn, cursor = db_function.dbConection()
+
+    if (verificarId):
+        cursor.execute("""SELECT * FROM public.{} WHERE id = {};""".format(nameTable,verificarId))
+    else:
+        cursor.execute("""SELECT * FROM public.{};""".format(nameTable))
+
+    nomesColunas = [desc[0] for desc in cursor.description]
+    valoresDb = cursor.fetchall()
+    
+    print(tabulate(valoresDb, headers=nomesColunas, tablefmt='grid'))
+
+    cursor.close()
+    conn.close()
+
+
+# UPDATE
+def dbUpdatePost(dadosAtualizarPosts,dbColumns,idAtualizar):
+    
+    conn, cursor = db_function.dbConection()
+
+    dadosAtualizarPostComAspas = ["'{}'".format(item) for item in dadosAtualizarPosts]
+
+    cursor.execute("""UPDATE public.{} SET {} WHERE id = {};""".format(nameTable,", ".join(f"{dbColumn} = {dadosAtualizarPostComAspa}" for dbColumn, dadosAtualizarPostComAspa in zip(dbColumns, dadosAtualizarPostComAspas)),int(idAtualizar)))
+     
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+# DELETE
+def dbDeletePost(idDeletar):
+    
+    conn, cursor = db_function.dbConection()
+
+    cursor.execute("""DELETE FROM public.{} WHERE id = {};""".format(nameTable,int(idDeletar)))
+     
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+
+def main():
+
+    def percorrerInputs(labelsInputs):
+        inputsValues = []
+        for inputs in labelsInputs:
+            inputsValues.append(input("Digite o {}:".format(inputs)))
+
+        return inputsValues
+    
+    interfaceVerificar = True
+
+    while interfaceVerificar:
+        opcaoInterface = input('1 - Criar Post\n2 - Ler Posts\n3 - Editar Post\n4 - Deletar Post\n5 - Fechar Interface\nEscolha uma opção:')
+        limpar_console()
+        match opcaoInterface:
+            case '1':
+                dbCriarPost(percorrerInputs(columnsTable),columnsTable)
+                limpar_console()
+            case '2':
+                dbLerPosts()
+                sairLeitura = input("\n Aperte Enter para voltar ao menu.")
+                limpar_console()
+            case '3':
+                dbLerPosts()
+                idAtualizar = input("\nDigite o ID para ATUALIZAR:")
+                dbUpdatePost(percorrerInputs(columnsTable),columnsTable,idAtualizar)
+                limpar_console()
+                print("Dados atualizados:")
+                dbLerPosts(idAtualizar)
+                sairLeitura = input("\n Aperte Enter para voltar ao menu.")
+                limpar_console()
+            case '4':
+                dbLerPosts()
+                idDeletar = input("\nDigite o ID para DELETAR:")
+                limpar_console()
+                dbDeletePost(idDeletar)
+            case '5':
+                interfaceVerificar = False
+main()
+
+
+
---
+++ db_function.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+# CONEXÃO BANCO DE DADOS:
+# PRECISA ALTERAR AS CREDENCIAIS DO SEU BANCO.
+
+def dbConection():
+    conn = psycopg2.connect(
+        database="postgres",
+        host="localhost",
+        user="postgres",
+        password="123",
+        port="5432"
+    )
+
+    cursor = conn.cursor()
+    return conn, cursor
+
+# FUNÇÃO QUE CRIA TABELA CASO ELA NÃO EXISTA, E ADICIONA UM ID COMO SERIAL NA CRIAÇÃO DA TABELA. ELA IRÁ CRIAR TODAS AS COLUNAS COMO TEXT
+
+def dbCreateTable(nameTable, columnsTable):
+
+    conn, cursor = dbConection()
+
+    dbColumns = "id serial primary key, "
+
+    dbColumns = dbColumns + ", ".join([f"{coluna} TEXT" for coluna in columnsTable])
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS public.{} ({});""".format(nameTable,dbColumns))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
